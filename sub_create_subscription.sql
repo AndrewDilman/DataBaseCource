1971e55a4dd8959c7c776acfa369828be137d311
AndrewD <andreidliman2004@mail.ru>
2025-12-10 13:13:12 +0700

    донастройка дампов и запуска

--- sub_create_subscription.sql
+++ sub_create_subscription.sql
@@ -1,23 +1,18 @@
--- Создание подписки на логическую публикацию с primary
--- Этот скрипт выполняется на контейнере postgres_subscriber
--- Требует доступности primary по сети как хоста "postgres" (имя сервиса в Compose)
+-- Заглушка для подписки
+-- Реальная подписка будет создана с помощью скрипта setup_replication.sh
+-- после запуска контейнеров, когда основная база данных будет готова
+-- и сможет принимать подключения для создания подписки
 
 -- Удалим подписку, если существует (для идемпотентности при перезапусках)
 DO $$
 BEGIN
     IF EXISTS (SELECT 1 FROM pg_subscription WHERE subname = 'marketplace_all_tables_sub') THEN
-        EXECUTE 'DROP SUBSCRIPTION marketplace_all_tables_sub';
+        EXECUTE 'DROP SUBSCRIPTION IF EXISTS marketplace_all_tables_sub';
     END IF;
 END $$;
 
--- Создаём подписку, копируем исторические данные (copy_data = true)
-CREATE SUBSCRIPTION marketplace_all_tables_sub
-CONNECTION 'host=postgres port=5432 dbname=marketplace user=admin password=123'
-PUBLICATION marketplace_all_tables
-WITH (
-    copy_data = true,
-    create_slot = true,
-    enabled = true
-);
-
-
+-- Выводим информационное сообщение
+DO $$
+BEGIN
+    RAISE NOTICE 'Подписка будет создана отдельно с помощью скрипта setup_replication.sh';
+END $$;
